507087cbdb88c0b57225f99dde6c7940a001f4ad
LittleRockSttar <danillov.allina@gmail.com>
2023-02-04 23:43:34 +0200

    script.js mod

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const noutati = document.getElementById("noutati");
 
 noutati.addEventListener("mouseover", (event) => {
-    event.target.classList.toggle("active") 
+    event.target.classList.toggle("active")
+    setTimeout(() => {
+        event.target.classList.remove("active");
+      }, 2000); 
     });
+
+
 
 
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("mouseover", (event) => {
     event.target.classList.toggle("active") 
+    setTimeout(() => {
+        event.target.classList.remove("active");
+      }, 2000); 
     });
